a26968fd4b5b771ca2fc4bba269d72f273efc7f3
Panagiotis Papapanagiotou <ad15@adweb.local>
2023-06-26 13:34:25 +0300

    test build 1

---
+++ build/precache-manifest.bed2f0c587cdef6f445b352eb26f67de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c77bde5f96f9c4e32e705d694c67fe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c857ab6513a3e3f14a06",
+    "url": "/static/css/main.5b681506.chunk.css"
+  },
+  {
+    "revision": "bc2588ecff3e1de8ec2b",
+    "url": "/static/js/2.7bd158f9.chunk.js"
+  },
+  {
+    "revision": "7c9439524938416d690cd438ba20d8b7",
+    "url": "/static/js/2.7bd158f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c857ab6513a3e3f14a06",
+    "url": "/static/js/main.751c7d32.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.751c7d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda552d77a08a0a69203",
+    "url": "/static/js/runtime-main.ea3aaf5f.js"
+  }
+]);
