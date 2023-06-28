a8539288ace6d7179b57620d3e6b794c29e5ed13
Panagiotis Papapanagiotou <ad15@adweb.local>
2023-06-28 10:14:33 +0300

    Updates

---
+++ precache-manifest.a6f334adaa6d7cb4d1544494df47115c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb892b2460f51dd166d847e5ad3de87",
+    "url": "/ad-gallery/index.html"
+  },
+  {
+    "revision": "e9e2db436dde853f586c",
+    "url": "/ad-gallery/static/css/main.4af60dab.chunk.css"
+  },
+  {
+    "revision": "b1c593ae690f344d4b98",
+    "url": "/ad-gallery/static/js/2.3a54bd8c.chunk.js"
+  },
+  {
+    "revision": "7c9439524938416d690cd438ba20d8b7",
+    "url": "/ad-gallery/static/js/2.3a54bd8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e2db436dde853f586c",
+    "url": "/ad-gallery/static/js/main.89283272.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/ad-gallery/static/js/main.89283272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f013959704f66bd299",
+    "url": "/ad-gallery/static/js/runtime-main.0bf1ffcb.js"
+  }
+]);
