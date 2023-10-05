402b50a40b5e6be4ffb7152e244e93d9a63ebb0d
Panagiotis Papapanagiotou <ad15@adweb.local>
2023-10-05 11:08:41 +0300

    Updates

--- precache-manifest.7fdb181c0203ac92b33db0dbf37581bd.js
+++ precache-manifest.7fdb181c0203ac92b33db0dbf37581bd.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7c3eebd05782f53b3fe9d1daf88dd088",
+    "revision": "ba5497f1789d14f1de5eaf811084ec25",
     "url": "/ad-gallery/index.html"
   },
   {
-    "revision": "638c415ae519306e3b9f",
+    "revision": "b6fb6f37c81dc819b7a1",
     "url": "/ad-gallery/static/css/main.8385d47f.chunk.css"
   },
   {
@@ -16,12 +16,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ad-gallery/static/js/2.f81a42f3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "638c415ae519306e3b9f",
-    "url": "/ad-gallery/static/js/main.938a9a99.chunk.js"
+    "revision": "b6fb6f37c81dc819b7a1",
+    "url": "/ad-gallery/static/js/main.a8e05da3.chunk.js"
   },
   {
     "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
-    "url": "/ad-gallery/static/js/main.938a9a99.chunk.js.LICENSE.txt"
+    "url": "/ad-gallery/static/js/main.a8e05da3.chunk.js.LICENSE.txt"
   },
   {
     "revision": "07f013959704f66bd299",
